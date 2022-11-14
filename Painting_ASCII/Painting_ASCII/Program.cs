4dba90d8be801c8d4e1ed9ca1e83180677946081
Калюх Павло <114071810+Saly-X@users.noreply.github.com>
2022-11-14 22:35:36 +0200

    Add files via upload

---
+++ Painting_ASCII/Painting_ASCII/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Paint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of mirror");
+            int size = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < size; i++)
+            {
+                string c = "";
+                string s = "  ";
+
+                for (int j = 0; j < i * 4; j++)
+                {
+                    c += ".";
+                }
+                for (int j = (size - i - 2) * 2; j >= 0; j--)
+                {
+                    s += " ";
+                }
+                Console.Write(s + "|<>");
+                Console.WriteLine(c + "<>|");
+            }
+            for (int i = size - 1; i >= 0; i--)
+            {
+                string c = "";
+                string s = "  ";
+                for (int j = 0; j < i * 4; j++)
+                {
+                    c += ".";
+                }
+                for (int j = 0; j <= (size - i - 2) * 2; j++)
+                {
+                    s += " ";
+                }
+                Console.Write(s + "|<>");
+                Console.WriteLine(c + "<>|");
+            }
+        }
+    }
+}
---
+++ Pascal's_triangle/Pascal's_triangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PascalTriangle
+{
+
+    class PascalTriangle
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pascal Triangle Program");
+            Console.Write("Enter the number of rows: ");
+            string input = Console.ReadLine();
+
+            int n = Convert.ToInt32(input);
+
+
+            for (int y = 0; y < n; y++)
+            {
+                int c = 1;
+                for (int q = 0; q < n - y; q++)
+                {
+                    Console.Write("   ");
+                }
+
+                for (int x = 0; x <= y; x++)
+                {
+                    Console.Write("   {0:D}  ", c);
+                    c = c * (y - x) / (x + 1);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
